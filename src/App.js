4b9fd68b243d008c249109a23ca17f82a55d7298
rana <trana51997@gmail.com>
2022-11-03 02:35:08 -0500

    Done with adding and removing state to modal when cart is clicked

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
+import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
+
+  const showModalHandler = () => {
+    setShowModal(true);
+  };
+
+  const closeModalHandler = () => {
+    setShowModal(false);
+  };
+
   return (
     <Fragment>
-      <Header />
+      {showModal && <Cart onCloseModal={closeModalHandler} />}
+      <Header onShowModal={showModalHandler} />
       <main>
         <Meals />
       </main>
