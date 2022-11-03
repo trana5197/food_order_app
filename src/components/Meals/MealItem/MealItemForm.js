deaeec418da3113c6c13c196cf864b229ae085fb
rana <trana51997@gmail.com>
2022-11-03 05:16:50 -0500

    Added Context and Reducer and used logic to implement cart modal and add button at meals section

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,21 +1,46 @@
+import { useRef, useState } from "react";
 import Input from "../../UI/Input";
 import classes from "./MealItemForm.module.css";
 
 const MealItemForm = (props) => {
+  const amountInputRef = useRef();
+  const [amountIsValid, setAmountIsValid] = useState(true);
+
+  const submitFormHandler = (event) => {
+    event.preventDefault();
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+
+    setAmountIsValid(true);
+    props.onAddToCart(enteredAmountNumber);
+    // console.log(enteredAmountNumber);
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitFormHandler}>
       <Input
+        ref={amountInputRef}
         label="Amount"
         input={{
           type: "number",
           id: "amount_" + props.id,
-          min: "1",
-          max: "5",
+          // min: "1",
+          // max: "5",
           step: "1",
           defaultValue: "1",
         }}
       />
       <button>+ Add</button>
+      {!amountIsValid && <p>Please enter a amount (1-5)</p>}
     </form>
   );
 };
